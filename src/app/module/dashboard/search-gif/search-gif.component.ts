eaf402289e34200e26df2d1d5ac7357090094b53
Aaron <radees.24.16@gmail.com>
2022-05-17 09:58:06 -0500

    debonceTime

--- src/app/module/dashboard/search-gif/search-gif.component.ts
+++ src/app/module/dashboard/search-gif/search-gif.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { APIGIFService } from './../../../shared/service/apigif.service';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'form-search-gif',
@@ -10,13 +11,16 @@ import { APIGIFService } from './../../../shared/service/apigif.service';
 export class SearchGifComponent implements OnInit {
 
   currenField = new FormControl();
+  results: any[] = [];
 
   constructor(private readonly apigifService: APIGIFService) { }
 
   ngOnInit(): void {
-    this.currenField.valueChanges.subscribe(resp => {
+    this.currenField.valueChanges
+    .pipe(debounceTime(300))
+    .subscribe(resp => {
       this.apigifService.getData(resp).subscribe(res => {
-        console.log(res);
+        this.results = res;
       });
     });
   }
